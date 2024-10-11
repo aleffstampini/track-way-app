f452d591745d997e21e0cbf4468d9357c4f78f8b
aleffstampini <aleffkelvin16@gmail.com>
2024-10-11 01:40:54 -0300

    build: init project

---
+++ src/main/java/br/com/track_way_app/TrackWayAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.track_way_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackWayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackWayAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/track_way_app/TrackWayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.track_way_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackWayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
