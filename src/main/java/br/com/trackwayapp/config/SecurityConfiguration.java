d392d77d7e642b572c1dd768b4a37b6cd32e91f4
aleffstampini <aleffkelvin16@gmail.com>
2024-10-15 01:01:09 -0300

    fix: configure spring security endpoints

---
+++ src/main/java/br/com/trackwayapp/config/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package br.com.trackwayapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authz -> authz.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
