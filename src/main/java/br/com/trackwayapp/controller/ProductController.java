f88b49e0e7019a99df29c0dac58318a2ffa0ce02
aleffstampini <aleffkelvin16@gmail.com>
2024-10-15 07:18:36 -0300

    feat: add swagger

--- src/main/java/br/com/trackwayapp/controller/ProductController.java
+++ src/main/java/br/com/trackwayapp/controller/ProductController.java
@@ -3,6 +3,11 @@ package br.com.trackwayapp.controller;
 import br.com.trackwayapp.dto.response.ProductHistoryDetailResponseDto;
 import br.com.trackwayapp.dto.response.ProductHistoryResponseDto;
 import br.com.trackwayapp.service.ProductHistoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,11 +22,27 @@ public class ProductController {
 
     private final ProductHistoryService productHistoryService;
 
+    @Operation(summary = "Obter histórico do produto", description = "Retorna o histórico do produto especificado pelo ID")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Histórico retornado com sucesso",
+            content = @Content(mediaType = "application/json",
+            schema = @Schema(implementation = ProductHistoryResponseDto.class))),
+        @ApiResponse(responseCode = "404", description = "Produto não encontrado",
+            content = @Content)
+    })
     @GetMapping("/{productId}/history")
     public ResponseEntity<ProductHistoryResponseDto> getHistory(@PathVariable(name = "productId") Long productId) {
         return ResponseEntity.ok(this.productHistoryService.getProductHistory(productId));
     }
 
+    @Operation(summary = "Obter histórico detalhado do produto", description = "Retorna o histórico detalhado do produto especificado pelo ID")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Histórico detalhado retornado com sucesso",
+            content = @Content(mediaType = "application/json",
+            schema = @Schema(implementation = ProductHistoryDetailResponseDto.class))),
+        @ApiResponse(responseCode = "404", description = "Produto não encontrado",
+            content = @Content)
+    })
     @GetMapping("/{productId}/history/details")
     public ResponseEntity<ProductHistoryDetailResponseDto> getHistoryDetails(
             @PathVariable(name = "productId") Long productId) {
